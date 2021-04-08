89bc897e7bd4d6ed24d58f8670d2d2e86d3d52c5
Lui Pillmann <lui.pillmann@gmail.com>
2021-04-07 22:24:38 -0300

    Add black to project and apply to py files

--- etl.py
+++ etl.py
@@ -26,61 +26,59 @@ def execute(conn, cur, query, data):
             conn.rollback()
         # keep raising for other cases of NotNullViolation occurrences
         else:
-            raise e 
+            raise e
 
 
 def process_song_file(conn, cur, filepath):
     """Process song file to insert songs and artists data"""
     # read contents of the file
-    with open(filepath, 'r') as f:
+    with open(filepath, "r") as f:
         file_contents = json.load(f)
-    
+
     # open song file
     df = pd.DataFrame.from_records(data=[file_contents], columns=file_contents.keys())
 
     # insert song record
-    song_data = (
-        df[["song_id", "title", "artist_id", "year", "duration"]]
-        .values
-        .tolist()
-        [0]
-    ) 
+    song_data = df[
+        ["song_id", "title", "artist_id", "year", "duration"]
+    ].values.tolist()[0]
     execute(conn, cur, song_table_insert, song_data)
-    
+
     # insert artist record
-    artist_data = (
-        df[["artist_id", "artist_name", "artist_location", "artist_latitude", "artist_longitude"]]
-        .values
-        .tolist()
-        [0]
-    ) 
+    artist_data = df[
+        [
+            "artist_id",
+            "artist_name",
+            "artist_location",
+            "artist_latitude",
+            "artist_longitude",
+        ]
+    ].values.tolist()[0]
     execute(conn, cur, artist_table_insert, artist_data)
 
 
 def process_log_file(conn, cur, filepath):
     """Process log file to insert time, users and songplays data"""
     # open log file
-    file_contents = [json.loads(line) for line in open(filepath, 'r')]
+    file_contents = [json.loads(line) for line in open(filepath, "r")]
     df = pd.DataFrame.from_records(data=file_contents, columns=file_contents[0].keys())
 
     # filter by NextSong action and convert timestamp column to datetime
-    df = (
-        df
-        .query("page == 'NextSong'")
-        .assign(ts=lambda x: pd.to_datetime(x['ts'], unit='ms'))
+    df = df.query("page == 'NextSong'").assign(
+        ts=lambda x: pd.to_datetime(x["ts"], unit="ms")
     )
 
     # build time dataframe from timestamp column
     time_df = (
-        df['ts']
+        df["ts"]
         .to_frame()
-        .rename(columns={'ts': 'start_time'})
-        .assign(hour=lambda x: x['start_time'].dt.hour)
-        .assign(day=lambda x: x['start_time'].dt.day)
-        .assign(week=lambda x: x['start_time'].dt.isocalendar().week)
-        .assign(month=lambda x: x['start_time'].dt.month)
-        .assign(year=lambda x: x['start_time'].dt.year)
-        .assign(weekday=lambda x: x['start_time'].dt.weekday)
+        .rename(columns={"ts": "start_time"})
+        .assign(hour=lambda x: x["start_time"].dt.hour)
+        .assign(day=lambda x: x["start_time"].dt.day)
+        .assign(week=lambda x: x["start_time"].dt.isocalendar().week)
+        .assign(month=lambda x: x["start_time"].dt.month)
+        .assign(year=lambda x: x["start_time"].dt.year)
+        .assign(weekday=lambda x: x["start_time"].dt.weekday)
     )
 
     # insert time data records
@@ -88,23 +86,21 @@ def process_log_file(conn, cur, filepath):
         execute(conn, cur, time_table_insert, list(row))
 
     # load user table
-    user_df = (
-        df[['userId', 'firstName', 'lastName', 'gender', 'level']]
-        .drop_duplicates()
-    )
+    user_df = df[
+        ["userId", "firstName", "lastName", "gender", "level"]
+    ].drop_duplicates()
 
     # insert user records
     for _, row in user_df.iterrows():
         execute(conn, cur, user_table_insert, row)
 
-    
     # insert songplay records
     for _, row in df.iterrows():
-        
+
         # get songid and artistid from song and artist tables
         execute(conn, cur, song_select, (row.song, row.artist, row.length))
         results = cur.fetchone()
-        
+
         if results:
             songid, artistid = results
         else:
@@ -113,14 +109,14 @@ def process_log_file(conn, cur, filepath):
         # insert songplay record
         songplay_data = (
             str(uuid.uuid4()),
-            pd.to_datetime(row['ts'], unit='ms'),
-            row['userId'],
-            row['level'],
+            pd.to_datetime(row["ts"], unit="ms"),
+            row["userId"],
+            row["level"],
             songid,
             artistid,
-            row['sessionId'],
-            row['location'],
-            row['userAgent'],
+            row["sessionId"],
+            row["location"],
+            row["userAgent"],
         )
         execute(conn, cur, songplay_table_insert, songplay_data)
 
@@ -130,28 +126,30 @@ def process_data(cur, conn, filepath, func):
     # get all files matching extension from directory
     all_files = []
     for root, _, files in os.walk(filepath):
-        files = glob.glob(os.path.join(root,'*.json'))
-        for f in files :
+        files = glob.glob(os.path.join(root, "*.json"))
+        for f in files:
             all_files.append(os.path.abspath(f))
 
     # get total number of files found
     num_files = len(all_files)
-    print('{} files found in {}'.format(num_files, filepath))
+    print("{} files found in {}".format(num_files, filepath))
 
     # iterate over files and process
     for i, datafile in enumerate(all_files, 1):
         func(conn, cur, datafile)
         # conn.commit()
-        print('{}/{} files processed.'.format(i, num_files))
+        print("{}/{} files processed.".format(i, num_files))
 
 
 def main():
     """Scrip entrypoint"""
-    conn = psycopg2.connect("host=127.0.0.1 dbname=sparkifydb user=postgres password=1234")
+    conn = psycopg2.connect(
+        "host=127.0.0.1 dbname=sparkifydb user=postgres password=1234"
+    )
     cur = conn.cursor()
 
-    process_data(cur, conn, filepath='data/song_data', func=process_song_file)
-    process_data(cur, conn, filepath='data/log_data', func=process_log_file)
+    process_data(cur, conn, filepath="data/song_data", func=process_song_file)
+    process_data(cur, conn, filepath="data/log_data", func=process_log_file)
 
     conn.close()
 
--- sql_queries.py
+++ sql_queries.py
@@ -1,4 +1,3 @@
-
 # DROP TABLES
 songplay_table_drop = "DROP TABLE IF EXISTS songplays"
 user_table_drop = "DROP TABLE IF EXISTS users"
@@ -8,7 +7,7 @@ time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
 
-songplay_table_create = ("""
+songplay_table_create = """
     CREATE TABLE songplays (
         songplay_id text NOT NULL,
         start_time timestamp with time zone,
@@ -27,9 +26,9 @@ songplay_table_create = ("""
             session_id
         )
     )
-""")
+"""
 
-user_table_create = ("""
+user_table_create = """
     CREATE TABLE users (
         user_id text PRIMARY KEY,
         first_name text,
@@ -37,9 +36,9 @@ user_table_create = ("""
         gender text,
         level text
     )
-""")
+"""
 
-song_table_create = ("""
+song_table_create = """
     CREATE TABLE songs (
         song_id text PRIMARY KEY,
         title text,
@@ -47,9 +46,9 @@ song_table_create = ("""
         year integer,
         duration float
     )
-""")
+"""
 
-artist_table_create = ("""
+artist_table_create = """
     CREATE TABLE artists (
         artist_id text PRIMARY KEY,
         name text,
@@ -57,9 +56,9 @@ artist_table_create = ("""
         latitude float,
         longitude float
     )
-""")
+"""
 
-time_table_create = ("""
+time_table_create = """
     CREATE TABLE time (
         start_time timestamp with time zone,
         hour integer,
@@ -69,39 +68,39 @@ time_table_create = ("""
         year integer,
         weekday text
     )
-""")
+"""
 
 # INSERT RECORDS
 
-songplay_table_insert = ("""
+songplay_table_insert = """
     INSERT INTO songplays (songplay_id, start_time, user_id, level, song_id, artist_id, session_id, location, user_agent)
     VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s)
-""")
+"""
 
-user_table_insert = ("""
+user_table_insert = """
     INSERT INTO users (user_id, first_name, last_name, gender, level)
     VALUES (%s, %s, %s, %s, %s)
-""")
+"""
 
-song_table_insert = ("""
+song_table_insert = """
     INSERT INTO songs (song_id, title, artist_id, year, duration) 
     VALUES (%s, %s, %s, %s, %s)
-""")
+"""
 
-artist_table_insert = ("""
+artist_table_insert = """
     INSERT INTO artists (artist_id, name, location, latitude, longitude) 
     VALUES (%s, %s, %s, %s, %s)
-""")
+"""
 
 
-time_table_insert = ("""
+time_table_insert = """
     INSERT INTO time (start_time, hour, day, week, month, year, weekday) 
     VALUES (%s, %s, %s, %s, %s, %s, %s)
-""")
+"""
 
 # FIND SONGS
 
-song_select = ("""
+song_select = """
     select
         s.song_id,
         a.artist_id
@@ -115,9 +114,21 @@ song_select = ("""
         a.name = %s
         and
         s.duration = %s
-""")
+"""
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
-drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
+create_table_queries = [
+    songplay_table_create,
+    user_table_create,
+    song_table_create,
+    artist_table_create,
+    time_table_create,
+]
+drop_table_queries = [
+    songplay_table_drop,
+    user_table_drop,
+    song_table_drop,
+    artist_table_drop,
+    time_table_drop,
+]
